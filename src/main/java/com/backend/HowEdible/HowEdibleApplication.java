93231335c1236258ea311199307837e0c1244a25
han18 <han.fadah@gmail.com>
2025-04-06 13:57:54 -0400

    updating project info

--- src/main/java/com/backend/HowEdible/HowEdibleApplication.java
+++ src/main/java/com/backend/HowEdible/HowEdibleApplication.java
@@ -13,7 +13,11 @@ public class HowEdibleApplication {
 		System.out.println(System.getProperty("java.version"));
 		
 //		to check the server is running 
+		System.out.println("This project is the original version");
+		System.out.println("This project has a git and a github repositiory called Backend-HowEdible");
+
 		System.out.println("Server is running successfully ");
+
 		
 	}
 
